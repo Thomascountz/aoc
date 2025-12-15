aef3b6b43518755b42619443a0c505ab6fe11c75
Thomas Countz <thomascountz@gmail.com>
2025-12-15 20:31:35 +0100

    01 - refactor

--- 2025/01.go
+++ 2025/01.go
@@ -10,60 +10,48 @@ import (
 )
 
 func main() {
-	var dial int64 = 50
+	dial := 50
 	zeroCount := 0
-
 	path := filepath.Join("input", "01.txt")
-	file, err := os.Open(path)
 
+	file, err := os.Open(path)
 	if err != nil {
 		panic("error reading file")
 	}
-
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 
-	fmt.Println("Instruction\tDial\tZeroCount")
-	// fmt.Println("----------------------------------")
-
 	for scanner.Scan() {
 		instruction := scanner.Text()
 
+		clicks, err := strconv.Atoi(instruction[1:])
+		if err != nil {
+			panic("parse fail")
+		}
+
 		if s.HasPrefix(instruction, "L") {
-			clicksInstruction := s.TrimPrefix(instruction, "L")
-			clicks, err := strconv.ParseInt(clicksInstruction, 10, 64)
-			if err != nil {
-				panic("parse fail")
-			}
 			dial = mod(dial-clicks, 100)
-			if dial == 0 {
-				zeroCount++
-			}
 		} else if s.HasPrefix(instruction, "R") {
-			clicksInstruction := s.TrimPrefix(instruction, "R")
-			clicks, err := strconv.ParseInt(clicksInstruction, 10, 64)
-			if err != nil {
-				panic("parse fail")
-			}
 			dial = mod(dial+clicks, 100)
-			if dial == 0 {
-				zeroCount++
-			}
 		} else {
 			panic("unreachable")
 		}
 
-		// fmt.Printf("%s\t\t%d\t\t%d\n", instruction, dial, zeroCount)
+		if dial == 0 {
+			zeroCount++
+		}
+
+		fmt.Printf("%s\tdial:%4d\t%4d\n", instruction, dial, zeroCount)
 
 	}
 
-	fmt.Println("----------------------------------")
-	fmt.Printf("\t\t%d\t\t%d\n", dial, zeroCount)
+	fmt.Printf("\nDial: %d\tZeros: %d\n", dial, zeroCount)
 }
 
-func mod(a, b int64) int64 {
-	return ((a % b) + b) % b
+func mod(a, b int) int {
+	// return ((a % b) + b) % b
+	return a % b
 }
 
 // Answers
